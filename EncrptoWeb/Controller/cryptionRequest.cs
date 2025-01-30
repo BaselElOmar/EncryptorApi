9d6893558d132d6ec50e80271472f301a3966629
BaselElOmar <Elomarbasel@gmail.com>
2025-01-30 22:18:51 +0100

    Revert "Revert 5 yamlfix"

---
+++ EncrptoWeb/Controller/cryptionRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CryptoWeb.Models
+{
+    public class cryptionRequest
+    {
+        public string ? Text { get; set; }
+        public string ? Key { get; set; }
+    }
+}
